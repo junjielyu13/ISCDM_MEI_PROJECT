1143839573eb4ec802848330d7eb7e0ae97b5f3a
junjie li <jfjunjieli@gmail.com>
2025-06-04 11:50:47 +0200

    update

---
+++ webapp/src/main/java/crypto/XMLCrypto.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crypto;
+
+/**
+ *
+ * @author alumne
+ */
+public class XMLCrypto {
+    private static final String secretPaht = "/home/alumne/ISCDM_MEI_PROJECT/webapp/src/main/java/crypto/secret.key";
+
+}
